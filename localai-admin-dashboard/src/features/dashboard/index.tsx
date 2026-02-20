30543e17edffe186b7e34cd550f37e2d2fcfb2cd
Nick Yeager <yeag123@gmail.com>
2026-02-19 20:48:42 -0700

    (feat): update the theme

--- localai-admin-dashboard/src/features/dashboard/index.tsx
+++ localai-admin-dashboard/src/features/dashboard/index.tsx
@@ -74,21 +74,6 @@ export default function Dashboard() {
           defaultValue='overview'
           className='space-y-4'
         >
-          <div className='w-full overflow-x-auto pb-2'>
-            <TabsList>
-              <TabsTrigger value='overview'>Overview</TabsTrigger>
-              <TabsTrigger value='analytics' disabled>
-                Analytics
-              </TabsTrigger>
-              <TabsTrigger value='reports' disabled>
-                Reports
-              </TabsTrigger>
-              <TabsTrigger value='notifications' disabled>
-                Notifications
-              </TabsTrigger>
-            </TabsList>
-          </div>
-
           <TabsContent value='overview' className='space-y-4'>
             <div className='grid gap-4 sm:grid-cols-2 lg:grid-cols-4'>
               <StatCard
